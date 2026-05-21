8d4f0dac0f1c1c2e0a93320be319977205eb56cc
Wellington Souza <devwrsvieira@gmail.com>
2026-05-21 11:30:34 -0300

    enable database configuration

--- backend/src/main/java/com/barbershop/Application.java
+++ backend/src/main/java/com/barbershop/Application.java
@@ -1,16 +1,10 @@
+
 package com.barbershop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(
-    exclude = {
-        DataSourceAutoConfiguration.class,
-        SecurityAutoConfiguration.class
-    }
-)
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
